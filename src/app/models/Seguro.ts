7b51b75d7ffc80da6bead4c8bf9b634f54404904
lucas.chinarelli <lucas.chinarelli@rpinfo.com.br>
2021-06-28 00:45:00 -0300

    Teste ServiceWorker Angular

---
+++ src/app/models/Seguro.ts
@@ -0,0 +1,11 @@
+import { MarcaCarro } from './MarcaCarro';
+
+export class Seguro {
+    marcaCarro: MarcaCarro = <any>[];
+    modeloCarro: string = "";
+    placaCarro: string = "";
+    nomeProprietario: string = "";
+    sobrenomeProprietario: string = "";
+    dataNascimentoProprietario: string = "";
+}
+
